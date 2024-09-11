1bae7275c6fbe629483bdfeb214a9c859b5c7229
code-woncheol <codewc23@gmail.com>
2024-09-11 12:10:40 +0900

    0911upload
    0911upload

---
+++ src/main/webapp/js1.js
@@ -0,0 +1,10 @@
+/**
+ * 
+ */
+
+function over(obj) {
+	obj.src="https://image10.coupangcdn.com/image/ccm/banner/0c6eee57beb7fc485bb680d5abf1f3a3.jpg";
+}
+function out(obj) {
+	obj.src="https://static.coupangcdn.com/sa/cmg_paperboy/image/1725351853152/1920_450.jpg";
+}
